05f9c26cabdaca91157d2797fcddabfcb78c1603
Tunde <adesolaafeez222@gmail.com>
2022-10-13 17:44:10 -0700

    task4-6

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+/**
+ * main-Printing
+ *
+ * Return: Always 0 (successful)
+ */
+
+int main (void)
+{
+	puts ("\"programming is like building a multi lingual puzzle");
+	return (0);
+}
